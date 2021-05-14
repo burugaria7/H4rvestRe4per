71ae18cdc24ac36da0ae841c8f5f2a0fab4458f6
burugaria7 <burugaria@icloud.com>
2021-05-14 17:49:12 +0900

    DiscordBotstatusいじれるように、インスタンスだけ作ってセッター呼んだら変えれるよん

--- DiscordBot.py
+++ DiscordBot.py
@@ -12,18 +12,21 @@ class DiscordBotClass:
 
         self.status = "starting..."
 
-
         @self.client.event
         async def on_ready():
             # 起動したらターミナルにログイン通知が表示される
             print('ログインしました')
-            send_message_every_10sec.start()
+            send_message_every.start()
 
         @tasks.loop(seconds=30)
-        async def send_message_every_10sec():
+        async def send_message_every():
             print("a")
-            activity = discord.Activity(name=self.status,type=discord.ActivityType.custom)
-            await self.client.change_presence(status=discord.Status.idle, activity=activity)
+            activity = discord.Activity(name=str(self.status), type=1)
+            await self.client.change_presence(status=discord.Status.online, activity=activity)
+
+            # activityvar = discord.Activity(type=discord.ActivityType.custom, state="NAMEOFMYACTIVITY")
+            # await self.client.change_presence(activity=activityvar)
+
 
         # メッセージ受信時に動作する処理
         @self.client.event
@@ -34,7 +37,7 @@ class DiscordBotClass:
             # 「/neko」と発言したら「にゃーん」が返る処理
             if message.content == '/neko':
                 await message.channel.send('にゃーん')
-                activity = discord.Game(name="ABC", type=3)
+                activity = discord.Game(name="ABC",emoji=None, type=4)
                 await self.client.change_presence(status=discord.Status.idle, activity=activity)
 
         def maindisbot():
@@ -44,6 +47,8 @@ class DiscordBotClass:
         thread1 = threading.Thread(target=maindisbot)
         thread1.start()
 
+    def set_status(self, status):
+        self.status = status
 
     def send_msg(self, msg):
         webhook = DiscordWebhook(url=self.url, content=msg)
