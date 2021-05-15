20899ee8caa43b950a05d34bd82293e526285e44
burugaria7 <burugaria@icloud.com>
2021-05-15 22:04:24 +0900

    移譲先クラスにもセッター追加

--- NotificationCenter.py
+++ NotificationCenter.py
@@ -46,6 +46,10 @@ log_file = logging.FileHandler(class_name)
 logger.addHandler(log_file)
 
 
+def set_status(self, status):
+    discord_account_1.set_status(status)
+
+
 def notify_to_line(msg, level, account_no):
     level = "[" + level + "]"
     msg = level + "\n" + msg
