1ba5644bdfa26a0bf211f7e4ab935508b826c4c2
burugaria7 <burugaria@icloud.com>
2021-05-15 22:26:49 +0900

    DiscordStatusクラスfirstcommit

---
+++ DiscordStatus.py
@@ -0,0 +1,10 @@
+from discord_webhook import DiscordWebhook
+import discord
+import threading
+import asyncio
+import time
+from discord.ext import tasks
+
+class DiscordStatusClass:
+    def __init__(self):
+        pass
